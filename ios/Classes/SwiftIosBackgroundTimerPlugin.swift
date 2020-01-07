6167d3002145d6e359b26f91cec194bd3c2d048b
localadmin <localadmin@serly-d-imac.graphisoft.hu>
2020-01-07 20:54:30 +0100

    .

--- ios/Classes/SwiftIosBackgroundTimerPlugin.swift
+++ ios/Classes/SwiftIosBackgroundTimerPlugin.swift
@@ -1,7 +1,9 @@
 import Flutter
 import UIKit
 
-public class SwiftIosBackgroundTimerPlugin: NSObject, FlutterPlugin {
+public class SwiftIosBackgroundTimerPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
+  private var _eventSink: FlutterEventSink?
+
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "ios_background_timer", binaryMessenger: registrar.messenger())
     let instance = SwiftIosBackgroundTimerPlugin()
@@ -10,5 +12,15 @@ public class SwiftIosBackgroundTimerPlugin: NSObject, FlutterPlugin {
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     result("iOS " + UIDevice.current.systemVersion)
+  }
+
+  public func onListen (withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink ) -> FlutterError? {
+    _eventSink = events
+    return nil
+  }
+
+  public func onCancel (withArguments arguments: Any?) -> FlutterError? {
+    _eventSink = nil
+    return nil
   }
 }
